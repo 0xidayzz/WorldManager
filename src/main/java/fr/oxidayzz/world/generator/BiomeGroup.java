753b580ba220aeb03b7f9dd72d043659d2846421
Oxidayzz <couton.joh@gmail.com>
2026-04-23 19:34:55 +0200

    Feat: Ajout de nouveau biomes

--- src/main/java/fr/oxidayzz/world/generator/BiomeGroup.java
+++ src/main/java/fr/oxidayzz/world/generator/BiomeGroup.java
@@ -5,17 +5,24 @@ import java.util.Arrays;
 import java.util.List;
 
 public enum BiomeGroup {
-    OCEAN("ocean", Arrays.asList(Biome.OCEAN, Biome.DEEP_OCEAN, Biome.WARM_OCEAN, Biome.LUKEWARM_OCEAN)),
+    OCEAN("ocean", Arrays.asList(Biome.OCEAN, Biome.DEEP_OCEAN, Biome.WARM_OCEAN)),
     RIVIERE("riviere", Arrays.asList(Biome.RIVER, Biome.FROZEN_RIVER)),
-    MONTAGNE("montagne", Arrays.asList(Biome.JAGGED_PEAKS, Biome.STONY_PEAKS, Biome.FROZEN_PEAKS, Biome.MEADOW, Biome.GROVE)),
+    MONTAGNE("montagne", Arrays.asList(Biome.JAGGED_PEAKS, Biome.STONY_PEAKS, Biome.FROZEN_PEAKS, Biome.MEADOW)),
     NOIR("noir", Arrays.asList(Biome.DARK_FOREST)),
     CHENE("chene", Arrays.asList(Biome.FOREST, Biome.FLOWER_FOREST)),
-    BOULOT("boulot", Arrays.asList(Biome.BIRCH_FOREST, Biome.OLD_GROWTH_BIRCH_FOREST)),
-    JUNGLE("jungle", Arrays.asList(Biome.JUNGLE, Biome.BAMBOO_JUNGLE, Biome.SPARSE_JUNGLE)),
+    BOULOT("boulot", Arrays.asList(Biome.BIRCH_FOREST)),
+    JUNGLE("jungle", Arrays.asList(Biome.JUNGLE, Biome.BAMBOO_JUNGLE)),
     DESERT("desert", Arrays.asList(Biome.DESERT)),
     TAIGA("taiga", Arrays.asList(Biome.TAIGA, Biome.SNOWY_TAIGA)),
     GRANDE_TAIGA("grande_taiga", Arrays.asList(Biome.OLD_GROWTH_PINE_TAIGA, Biome.OLD_GROWTH_SPRUCE_TAIGA)),
-    OR("or", Arrays.asList(Biome.BADLANDS, Biome.ERODED_BADLANDS, Biome.WOODED_BADLANDS));
+    OR("or", Arrays.asList(Biome.BADLANDS, Biome.ERODED_BADLANDS)),
+    
+    // --- NOUVEAUX BIOMES UTILES ---
+    PLAINE("plaine", Arrays.asList(Biome.PLAINS, Biome.SUNFLOWER_PLAINS)),
+    SAVANE("savane", Arrays.asList(Biome.SAVANNA, Biome.SAVANNA_PLATEAU, Biome.WINDSWEPT_SAVANNA)),
+    MARAIS("marais", Arrays.asList(Biome.SWAMP, Biome.MANGROVE_SWAMP)),
+    CERISIER("cerisier", Arrays.asList(Biome.CHERRY_GROVE)),
+    GROTTE("grotte", Arrays.asList(Biome.LUSH_CAVES, Biome.DRIPSTONE_CAVES));
 
     private final String key;
     private final List<Biome> biomes;
