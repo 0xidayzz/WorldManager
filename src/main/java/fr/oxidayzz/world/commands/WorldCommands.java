9fe3b24df37d76ea6de4fab7bcf6a19b42242066
Oxidayzz <couton.joh@gmail.com>
2026-04-23 17:23:16 +0200

    Feat: Ajout de la sous-commande /rw list avec formattage des environnements

--- src/main/java/fr/oxidayzz/world/commands/WorldCommands.java
+++ src/main/java/fr/oxidayzz/world/commands/WorldCommands.java
@@ -1,6 +1,9 @@
 package fr.oxidayzz.world.commands;
 
 import fr.oxidayzz.world.WorldManager;
+
+import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -28,6 +31,15 @@ public class WorldCommands implements CommandExecutor {
                 sender.sendMessage("§a§l[WorldManager] §7Version: §f" + plugin.getDescription().getVersion());
                 sender.sendMessage("§a§l[WorldManager] §7Auteur: §fOxidayzz");
                 break;
+            case "list":
+                sender.sendMessage("§8§m----------------§r §6§lMondes Actifs §8§m----------------");
+                // On boucle sur tous les mondes chargés par le serveur
+                for (World w : Bukkit.getWorlds()) {
+                    String environment = getEnvName(w.getEnvironment());
+                    sender.sendMessage("§8- §f" + w.getName() + " §7(" + environment + "§7)");
+                }
+                sender.sendMessage("§8§m--------------------------------------------");
+                break;    
 
             default:
                 sender.sendMessage("§cCommande inconnue. Tapez §6/rw help");
@@ -42,9 +54,17 @@ public class WorldCommands implements CommandExecutor {
         sender.sendMessage("§8§m----------------§r §6§lWorldManager §8§m----------------");
         sender.sendMessage(" ");
         sender.sendMessage("§6/rw help §8» §7Affiche ce menu d'aide.");
-        sender.sendMessage("§6/rw tp <monde> §8» §7Se téléporter ou créer un monde.");
+        sender.sendMessage("§6/rw list §8» §7Liste les mondes chargés.");
         sender.sendMessage("§6/rw info §8» §7Informations sur le plugin.");
         sender.sendMessage(" ");
         sender.sendMessage("§8§m--------------------------------------------");
+    }
+    private String getEnvName(World.Environment env) {
+        return switch (env) {
+            case NORMAL -> "§aOverworld";
+            case NETHER -> "§cNether";
+            case THE_END -> "§dEnd";
+            case CUSTOM -> "§bCustom";
+        };
     }
 }
